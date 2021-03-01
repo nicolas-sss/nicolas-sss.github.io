179c359b4f0813e364789a4b406ab557bb43005d
Nícolas Moura <nicolas_moura02@outlook.com>
2021-03-01 16:43:12 -0300

    Teste

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@
 // localStorage.setItem('userEmail', 'email@life.com');
 // localStorage.setItem('userPhone', '5581996906155');
 // localStorage.setItem('userDescription', 'Esta é uma descrição');
+function jivo_onOpen(){ 
+    console.log('teste'); 
+    jivo_api.setContactInfo({
+        name: localStorage.getItem('userName') ? localStorage.getItem('userName') : 'Usuário Padrão',
+        email: localStorage.getItem('userEmail') ? localStorage.getItem('userEmail') : 'padrao@email.com',
+        phone: localStorage.getItem('userPhone') ? localStorage.getItem('userPhone') : '+5581999999999',
+        description: localStorage.getItem('userDescription') ? localStorage.getItem('userDescription') : 'Usuário Padrão description'
+    });
+};
