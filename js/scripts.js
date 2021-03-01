6998f9c8d2f9a79073e71598a43f838ce0e247e8
Nícolas Moura <nicolas_moura02@outlook.com>
2021-03-01 16:40:22 -0300

    Teste

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-localStorage.setItem('userName', 'Gabi Valle');
-localStorage.setItem('userEmail', 'email@life.com');
-localStorage.setItem('userPhone', '5581996906155');
-localStorage.setItem('userDescription', 'Esta é uma descrição');
+// localStorage.setItem('userName', 'Gabi Valle');
+// localStorage.setItem('userEmail', 'email@life.com');
+// localStorage.setItem('userPhone', '5581996906155');
+// localStorage.setItem('userDescription', 'Esta é uma descrição');
